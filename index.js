6ffee40924442ea42362e4ccb1aa238de4a02bb7
Miguel <misha.pavlenko.1111@gmail.com>
2021-04-16 12:33:33 +0300

    NewCommit1

--- index.js
+++ index.js
@@ -7,28 +7,32 @@ const mongoUrl = "mongodb://localhost:27017/Tasks";
 let mongo;
 
 app.get('/', function (req, res) {
-    MongoClient
-        .connect(mongoUrl)
-        .then(function (client) {
-            mongo = client.db();
-/*             mongo
-                .collection('Tasks')
-                .insertOne({
-                    firstName: 'Іван',
-                    lastName: 'Іванов'
-                })
-                .then(function () {
-                    console.log('Запис створено');
-                }); */
-            mongo
-                .collection('Tasks')
-                .find()
-                .toArray()
-                .then(function (Tasks) {
-                    res.json(Tasks);
-                });
-        });
+    MongoClient.connect(mongoUrl).then(function (client) {
+        mongo = client.db();
+        /*             mongo
+                        .collection('Tasks')
+                        .insertOne({
+                            firstName: 'Іван',
+                            lastName: 'Іванов'
+                        })
+                        .then(function () {
+                            console.log('Запис створено');
+                        }); */
+        mongo
+            .collection('Tasks')
+            .find()
+            .toArray()
+            .then(function (Tasks) {
+                /* res.json(Tasks); */
+                res.render('hello', { result: Tasks});
+            });
+    });
+});
 
+app.set('view engine', 'ejs');
+
+app.get('/', function (req, res) {
+    res.render('hello');
 });
 
 app.listen(3000);
